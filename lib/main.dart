20a8c7736549bc53d25041165630cb343c944034
Mikael Carvalho <mcb3@discente.ifpe.edu.br>
2021-09-28 01:29:50 -0300

    Interface concluida

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,58 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Cadastro do avicultor',
+          ),
+        ),
+        backgroundColor: Colors.orangeAccent,
+        body: Center(
+          child: Column(
+            children: [
+              Text('Bem-Vindo', style: TextStyle(color: Colors.blue,
+                  fontSize: 30)),
+              Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(), labelText: 'Nome'),
+                  )),
+              Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(), labelText: 'Idade'),
+                  )),
+              Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(), labelText: 'Localização'),
+                  )),
+              Padding(
+                padding: const EdgeInsets.all(12),
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text('Cadastrar'),
+                  style: ElevatedButton.styleFrom(
+                      minimumSize: const Size.fromHeight(50)),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
